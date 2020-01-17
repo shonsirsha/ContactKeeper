0ca9dbec78cd05a878d476995fd3d15de6f16ae8
Sean Saoirse <seansaoirse@guo.jing_android>
2020-01-17 12:53:33 +0100

    Changed ports(2)

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const path = require("path");
+
 const app = express();
 
-// connect DB
-
+// Connect Database
 connectDB();
 
-//init middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-//Define routes
+// Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
-//serve static assets in prod
-
+// Serve static assets in production
 if (process.env.NODE_ENV === "production") {
+  // Set static folder
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server started on ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
