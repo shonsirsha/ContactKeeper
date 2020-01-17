a8a7e9ec2876c2555fd62d33d06163c8adaa1db6
Sean Saoirse <seansaoirse@guo.jing_android>
2020-01-17 12:50:02 +0100

    Changed port

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.port || 5000;
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server started on ${PORT}`);
 });
