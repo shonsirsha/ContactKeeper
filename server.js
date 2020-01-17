3e9af12d082f4e5ab2fb64c1820b25582a9deb90
Sean Saoirse <seansaoirse@guo.jing_android>
2020-01-17 12:42:32 +0100

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
 
 // connect DB
 
 connectDB();
-
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the contact keeper API." });
-});
 
 //init middleware
 app.use(express.json({ extended: false }));
@@ -17,6 +14,15 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+//serve static assets in prod
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.port || 5000;
 app.listen(PORT, () => {
